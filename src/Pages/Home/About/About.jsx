d1fc71324c7a6594d95f082f56e2dcb29cde3840
Nafim16 <dnafim081999@gmail.com>
2023-10-21 01:24:11 +0600

    service area 20%

--- src/Pages/Home/About/About.jsx
+++ src/Pages/Home/About/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
                 <div className="col-md-6">
                     <div className='mar'>
                         <h4 className='ah4'>About Us</h4>
-                        <h2 className='ah1'>We are qualified <br />& of experience <br />in this field</h2>
+                        <h3 className='ah1'>We are qualified <br />& of experience <br />in this field</h3>
                         <p className='ap1'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
                         <p className='ap2'>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
                         <button className='abs'>Get More Info</button>
---
+++ src/Pages/Home/Services/Services.jsx
@@ -0,0 +1,20 @@
+
+
+const Services = () => {
+    return (
+        <div className="mt-5 pt-5">
+            <div>
+                <div className="text-center">
+                    <h3 className="sh3">Our Services</h3>
+                    <h2 className="sh2">Our Service Area</h2>
+                    <p className="sp">the majority have suffered alteration in some form, by injected humour, or randomised <br />words which don't look even slightly believable. </p>
+                </div>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
